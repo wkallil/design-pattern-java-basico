85eecef68ce28f8f5efdd37600590028e757f1d2
Wkallil <kallil.tim@gmail.com>
2024-06-24 15:55:07 -0300

    Design Patterns com java básico

---
+++ one.dio.gof/src/facade/Facade.java
@@ -0,0 +1,15 @@
+package facade;
+
+import sistema1.crm.CrmService;
+import sistema2.cep.CepApi;
+
+public class Facade {
+
+    public void migrarCliente(String nome, String cep) {
+
+       String cidade = CepApi.getInstancia().recuperarCidade(cep);
+       String estado = CepApi.getInstancia().recuperarEstado(cep);
+
+       CrmService.gravarCliente(nome, cep, cidade, estado);
+    }
+}
---
+++ one.dio.gof/src/sistema2/cep/CepApi.java
@@ -0,0 +1,22 @@
+package sistema2.cep;
+
+public class CepApi {
+
+    private static CepApi instancia = new CepApi();
+
+    private CepApi() {
+        super();
+    }
+
+    public static CepApi getInstancia() {
+        return instancia;
+    }
+
+    public String recuperarCidade(String cep) {
+        return "Paulista";
+    }
+
+    public String recuperarEstado(String cep) {
+        return "PE";
+    }
+}
